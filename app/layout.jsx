ab8ccf3050b621f0fabb6968d496f7cf36546c78
contadorluidgabriel-cell <contadorluidgabriel@gmail.com>
2026-09-16 11:54:42 -0300

    refactor(importacoes): remove scattered global OFX launchers

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,4 @@
 import './globals.css';
-import OfxImportLauncher from '../components/OfxImportLauncher';
-import OfxEntryPostingLauncher from '../components/OfxEntryPostingLauncher';
 
 export const metadata = {
   title: 'Central Financeira',
@@ -10,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
-      <body>{children}<OfxImportLauncher/><OfxEntryPostingLauncher/></body>
+      <body>{children}</body>
     </html>
   );
 }
